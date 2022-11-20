44cd254dee8f66bcf426044775264ea03d184910
Kwame Ofori-Gyau <112716883+kgyau@users.noreply.github.com>
2022-11-20 21:11:08 +0000

    Update db_cred.php
    hjg

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,9 +1,9 @@
 <?php
 //Database credentials
-define('SERVERNAME', 'localhost');
-define('HOSTNAME', 'root');
-define('PASSWORD', '');
-define('DATABASE', 'chimera');
+define('SERVERNAME', 'us-cdbr-east-06.cleardb.net');
+define('HOSTNAME', 'b345c0e5c925f0');
+define('PASSWORD', '3a6ff711');
+define('DATABASE', 'heroku_54c070a3bc39030');
 
 
 ?>
