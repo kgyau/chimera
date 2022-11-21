70454728463105b729d13f2df5a84b2151ea0378
Kwame Ofori-Gyau <112716883+kgyau@users.noreply.github.com>
2022-11-21 09:33:05 +0000

    Update payment_process.php
    sd

--- actions/payment_process.php
+++ actions/payment_process.php
@@ -32,7 +32,7 @@ require("../controllers/cart_controller.php");
   curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
   //execute post
   $result = curl_exec($ch);
-  echo $result;
+//   echo $result;
 
     
     if ($result) {
@@ -42,23 +42,25 @@ require("../controllers/cart_controller.php");
     $status='Success';
     $payment_date=date('Y-m-d');
     $order= order_ctr($customer_id,$invoice_no,$payment_date,$status);
+     echo $customer_id,$invoice_no,$payment_date,$status;
 
-    if ($order) {
-        echo "success transaction";
-    }else {
-        echo "fail transaction";
-    }
+//     if ($order) {
+//         echo "success transaction";
+//     }else {
+//         echo "fail transaction";
+//     }
     //save payment details
     $order_id= order_id_ctr($invoice_no);
     $order_id=$order_id['order_id'];
     // $amt= $_GET['amt'];
     $currency= 'GHS';
     $payment= payment_ctr($amt,$customer_id,$order_id,$currency,$payment_date);
-    if ($payment) {
-      header("location:../view/all_prodcut.php");
-    }else {
-        echo "failed payment";
-    }
+      echo $amt,$customer_id,$order_id,$currency,$payment_date;
+//     if ($payment) {
+//       header("location:../view/all_prodcut.php");
+//     }else {
+//         echo "failed payment";
+//     }
 
 }
   ?>
